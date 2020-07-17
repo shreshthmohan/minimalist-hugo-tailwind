21fe19f575b7648e862da1ddb98db377134b79d2
Shreshth Mohan <sremog@gmail.com>
2020-07-17 16:05:59 +0530

    added empty footer. fixed styling in mobile

--- postcss.config.js
+++ postcss.config.js
@@ -1,15 +1,19 @@
-// const purgecss = require('@fullhuman/postcss-purgecss')({
-//   content: ['./layouts/**/*.html'],
-//   defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
-// })
+const purgecss = require("@fullhuman/postcss-purgecss")({
+  content: [
+    "./layouts/**/*.html",
+    "../../layouts/**/*.html",
+    "../../content/**/*.md",
+  ],
+  defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || [],
+});
 
 module.exports = {
   plugins: [
-    require('tailwindcss'),
-    require('autoprefixer'),
-    require('cssnano')
-    // ...process.env.NODE_ENV === 'production'
-    //   ? [purgecss, require('cssnano')]
-    //   : []
-  ]
-}
+    require("tailwindcss"),
+    require("autoprefixer"),
+    // require("cssnano"),
+    ...(process.env.NODE_ENV === "production"
+      ? [purgecss, require("cssnano")]
+      : []),
+  ],
+};
