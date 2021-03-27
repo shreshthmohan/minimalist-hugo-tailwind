eefb2318527f9f7def6decb6fcbc5f4a56ecf995
Shreshth Mohan <sremog@gmail.com>
2021-03-27 22:44:49 +0530

    dark mode almost done

--- static/js/dark.js
+++ static/js/dark.js
@@ -1,1 +1,60 @@
-console.log("hello from dark.js");
+(function () {
+  var storedTheme; // As set by user using the checkbox on the webpage
+  // theme signifies light or dark
+  try {
+    storedTheme = localStorage.getItem("theme");
+  } catch (err) {}
+
+  var darkQuery = window.matchMedia("(prefers-color-scheme: dark)");
+  var darkModeSwitch = document.getElementById("dark-mode-switch");
+
+  function makeDark() {
+    document.documentElement.classList.add("dark");
+    darkModeSwitch.checked = true;
+  }
+
+  function makeLight() {
+    document.documentElement.classList.remove("dark");
+    darkModeSwitch.checked = false;
+  }
+
+  if (storedTheme === "dark") {
+    makeDark();
+  } else if (storedTheme === "light") {
+    makeLight();
+  } else {
+    // use media query
+    if (darkQuery.matches) {
+      makeDark();
+    } else {
+      makeLight();
+    }
+  }
+  function darkQueryListen(e) {
+    if (e.matches) {
+      makeDark();
+    } else {
+      makeLight();
+    }
+  }
+  darkQuery.addEventListener("change", darkQueryListen);
+
+  function darkModeSwitchListen(e) {
+    var newThemeState = e.target.checked;
+    darkModeSwitch.checked = newThemeState;
+    if (newThemeState) {
+      makeDark();
+    } else {
+      makeLight();
+    }
+  }
+
+  darkModeSwitch.addEventListener("change", darkModeSwitchListen);
+
+  window.addEventListener("unload", function () {
+    console.log("removing listeners");
+    // probably unnecessary though
+    darkModeSwitch.removeEventListener("change", darkModeSwitchListen);
+    darkQuery.removeEventListener("change", darkQueryListen);
+  });
+})();
