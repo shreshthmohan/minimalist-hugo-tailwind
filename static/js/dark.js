8bb41f74ca92f490a4db0664e27bde8611407462
Shreshth Mohan <sremog@gmail.com>
2021-03-29 15:45:33 +0530

    Dark mode complete

--- static/js/dark.js
+++ static/js/dark.js
@@ -1,8 +1,8 @@
 (function () {
-  var storedTheme; // As set by user using the checkbox on the webpage
+  var darkTheme; // As set by user using the checkbox on the webpage
   // theme signifies light or dark
   try {
-    storedTheme = localStorage.getItem("theme");
+    darkTheme = localStorage.getItem("theme");
   } catch (err) {}
 
   var darkQuery = window.matchMedia("(prefers-color-scheme: dark)");
@@ -18,9 +18,9 @@
     darkModeSwitch.checked = false;
   }
 
-  if (storedTheme === "dark") {
+  if (darkTheme === 'true') {
     makeDark();
-  } else if (storedTheme === "light") {
+  } else if (darkTheme === 'false') {
     makeLight();
   } else {
     // use media query
@@ -42,6 +42,9 @@
   function darkModeSwitchListen(e) {
     var newThemeState = e.target.checked;
     darkModeSwitch.checked = newThemeState;
+    try {
+      localStorage.setItem("theme", newThemeState);
+    } catch (err) {}
     if (newThemeState) {
       makeDark();
     } else {
@@ -50,11 +53,4 @@
   }
 
   darkModeSwitch.addEventListener("change", darkModeSwitchListen);
-
-  window.addEventListener("unload", function () {
-    console.log("removing listeners");
-    // probably unnecessary though
-    darkModeSwitch.removeEventListener("change", darkModeSwitchListen);
-    darkQuery.removeEventListener("change", darkQueryListen);
-  });
 })();
