38e7d1539ba6b05e2587c3d9b773d4cfd7e3dfd1
Shreshth Mohan <sremog@gmail.com>
2021-10-17 16:57:39 +0530

    include static  js files for css gen

--- postcss.config.js
+++ postcss.config.js
@@ -1,20 +1,21 @@
-const purgecss = require("@fullhuman/postcss-purgecss")({
+const purgecss = require('@fullhuman/postcss-purgecss')({
   content: [
-    "./layouts/**/*.html",
-    "../../layouts/**/*.html",
-    "../../content/**/*.md",
-    "../../content/**/*.html",
+    './layouts/**/*.html',
+    '../../layouts/**/*.html',
+    '../../content/**/*.md',
+    '../../content/**/*.html',
+    '../../static/**/*.js',
   ],
-  defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || [],
-});
+  defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || [],
+})
 
 module.exports = {
   plugins: [
-    require("tailwindcss"),
-    require("autoprefixer"),
+    require('tailwindcss'),
+    require('autoprefixer'),
     // require("cssnano"),
-    ...(process.env.NODE_ENV === "production"
-      ? [purgecss, require("cssnano")]
+    ...(process.env.NODE_ENV === 'production'
+      ? [purgecss, require('cssnano')]
       : []),
   ],
-};
+}
